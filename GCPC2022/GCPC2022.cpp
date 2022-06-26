52dddacf0b76e9106e49e165eff0b5d38ab3f913
Ahmed Mousa <Ahmed.Mousa98@eng-st.cu.edu.eg>
2022-06-26 15:00:40 +0200

    adding some new filezs

--- GCPC2022/GCPC2022.cpp
+++ GCPC2022/GCPC2022.cpp
@@ -6,12 +6,15 @@ void main()
 	int x;
 	cin >> x;
 
+	bool myBoolArray[12] = { true };
+
+	cout <<"the value of the first elemt is " << myBoolArray[3] << endl;
 	if (x > 6)
 		cout << "Test hello world" << endl;
 	else
 		cout << "please enter a number that is bigger than 6" << endl;
 
 	return; 
-
+	
 }
 
